03a66c30816b483bad6cc792470e258295e01e78
EdgarGeminis13j <edgargeminis13j@gmail.com>
2025-10-04 19:54:33 -0500

    Se agrega pantalla para solicitar permisos de galería y menú de navegación lateral.

--- app/src/main/java/com/example/appdpa/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/appdpa/presentation/navigation/AppNavGraph.kt
@@ -1,5 +1,6 @@
 package com.example.appdpa.presentation.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -7,6 +8,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.appdpa.presentation.auth.LoginScreen
 import com.example.appdpa.presentation.auth.RegisterScreen
 import com.example.appdpa.presentation.home.HomeScreen
+import com.example.appdpa.presentation.permissions.GalleryPermissionScreen
 
 @Composable
 fun AppNavGraph(){
@@ -16,11 +18,28 @@ fun AppNavGraph(){
         composable("login"){
             LoginScreen(navController)
         }
+
         composable("register"){
             RegisterScreen(navController)
         }
+
         composable("home"){
-            HomeScreen()
+            DrawerScaffold(navController) {
+                HomeScreen()
+            }
         }
+
+        composable("permissions"){
+            DrawerScaffold(navController) {
+                GalleryPermissionScreen()
+            }
+        }
+
+        composable("favorites"){
+            DrawerScaffold(navController) {
+                Text("Proximamente pantalla de favoritos...")
+            }
+        }
+
     }
 }
